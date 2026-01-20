47061056934c78d4467f4d3ac298f3962a53c8aa
zwsimpso <zacksimpson2015@gmail.com>
2026-01-19 19:13:49 -0500

    Update Main and readME. Added json jar file.

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,28 @@
+import java.util.Scanner;
+
 void main(){
-    String name = "zack";
-    IO.println("hello " + name);
+
+
+    try{
+        //Create a scanner class to get user input
+        Scanner scanner = new Scanner(System.in);
+        String city;
+        String no = "No";
+
+        //Do while loop to allow users to check multiple cities, break out on "No"
+        do{
+            IO.println("///////////////");
+            IO.print("Please insert a city (No to quit): ");
+            city = scanner.nextLine();
+
+            if(city.equalsIgnoreCase(no)) break;
+
+            //put input into a string variable for easy printing
+            IO.println("hello " + city);
+        }while(!city.equalsIgnoreCase(no));
+
+    }catch(Exception e){
+        IO.println("Scanner error");
+    }
+
 }
